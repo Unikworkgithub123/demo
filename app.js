e0e8dda26ac80cf193c8523692744f04b42ddb8f
unikwork <unikworksystems@gmail.com>
2023-06-27 12:13:10 +0530

    scdsaf

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const { log } = require('console');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/user', async (req, res) => {
+    console.log("OKKKKKKK == Palty");
     res.status(200).json({
         success: true,
         name:"dinesh chauhan",
